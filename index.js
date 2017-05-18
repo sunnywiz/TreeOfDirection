e894c0e4d9a01aeb70dd7a57844ede5735d1b90a
Sunny Gulati <Sunny Gulati>
2017-05-17 22:52:37 -0400

    WIP: trying to get chains so i can parse out excess detail

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const bluebird = require('bluebird');
 const jscad = require('jscad');
+const polyline = require('polyline');
 const config = {
     origin: "8516 Brookside Drive West 40056",
-    desiredBounds: { min: [0, 0, 0], max: [100, 100, 50] },
+    desiredBounds: { min: [0, 100, 0], max: [100, 0, 50] },
     maxLatLng: [38.317138, -85.463906],
     minLatLng: [38.294255, -85.501226],
-    steps: [4, 4],
+    steps: [1, 1],
     printRadius: 1
 };
 const googleMapsClient = require('@google/maps').createClient({
@@ -13,38 +14,62 @@ const googleMapsClient = require('@google/maps').createClient({
     Promise: Promise
 });
 
-function doSomething(dataToPlot, result) {
+function addDirectionsToPlot(dataToPlot, result) {
     result.routes.forEach(route => {
         console.log("route: ", route.summary);
         var currentDuration = 0;
         route.legs.forEach(leg => {
+
+            var chain = [];
+
             leg.steps.forEach(step => {
+
+                if (step.travel_mode != "DRIVING") { 
+                    return; 
+                }
+
+                var polyLinePoints = polyline.decode(step.polyline.points);
+                var durationPerPoint = step.duration.value / (polyLinePoints.length - 1);
                 var newDuration = currentDuration + step.duration.value;
-                dataToPlot.push({
-                    start: [step.start_location.lat, step.start_location.lng, currentDuration],
-                    end: [step.end_location.lat, step.end_location.lng, newDuration]
-                });
+
+                for (var pi = 1; pi < polyLinePoints.length; pi++) {
+                    var x1 = polyLinePoints[pi - 1][0];
+                    var y1 = polyLinePoints[pi - 1][1];
+                    var x2 = polyLinePoints[pi][0];
+                    var y2 = polyLinePoints[pi][1];
+
+                    chain.push({
+                        start: [x1, y1, currentDuration],
+                        end: [x2, y2, currentDuration + durationPerPoint]
+                    });
+                    currentDuration += durationPerPoint;
+                }
+
                 currentDuration = newDuration;
             })
+
+            dataToPlot.push(chain);
         })
-    });
+    })
 }
 
 function getBounds(dataToPlot) {
-    var bounds = { min: dataToPlot[0].start.slice(0), max: dataToPlot[0].end.slice(0) };
-    dataToPlot.forEach(segment => {
-        if (segment.start[0] < bounds.min[0]) bounds.min[0] = segment.start[0];
-        if (segment.start[1] < bounds.min[1]) bounds.min[1] = segment.start[1];
-        if (segment.start[2] < bounds.min[2]) bounds.min[2] = segment.start[2];
-        if (segment.start[0] > bounds.max[0]) bounds.max[0] = segment.start[0];
-        if (segment.start[1] > bounds.max[1]) bounds.max[1] = segment.start[1];
-        if (segment.start[2] > bounds.max[2]) bounds.max[2] = segment.start[2];
-        if (segment.end[0] < bounds.min[0]) bounds.min[0] = segment.end[0];
-        if (segment.end[1] < bounds.min[1]) bounds.min[1] = segment.end[1];
-        if (segment.end[2] < bounds.min[2]) bounds.min[2] = segment.end[2];
-        if (segment.end[0] > bounds.max[0]) bounds.max[0] = segment.end[0];
-        if (segment.end[1] > bounds.max[1]) bounds.max[1] = segment.end[1];
-        if (segment.end[2] > bounds.max[2]) bounds.max[2] = segment.end[2];
+    var bounds = { min: dataToPlot[0][0].start.slice(0), max: dataToPlot[0][0].end.slice(0) };
+    dataToPlot.forEach(chain => {
+        chain.forEach(link => {
+            if (link.start[0] < bounds.min[0]) bounds.min[0] = link.start[0];
+            if (link.start[1] < bounds.min[1]) bounds.min[1] = link.start[1];
+            if (link.start[2] < bounds.min[2]) bounds.min[2] = link.start[2];
+            if (link.start[0] > bounds.max[0]) bounds.max[0] = link.start[0];
+            if (link.start[1] > bounds.max[1]) bounds.max[1] = link.start[1];
+            if (link.start[2] > bounds.max[2]) bounds.max[2] = link.start[2];
+            if (link.end[0] < bounds.min[0]) bounds.min[0] = link.end[0];
+            if (link.end[1] < bounds.min[1]) bounds.min[1] = link.end[1];
+            if (link.end[2] < bounds.min[2]) bounds.min[2] = link.end[2];
+            if (link.end[0] > bounds.max[0]) bounds.max[0] = link.end[0];
+            if (link.end[1] > bounds.max[1]) bounds.max[1] = link.end[1];
+            if (link.end[2] > bounds.max[2]) bounds.max[2] = link.end[2];
+        });
     });
     return bounds;
 }
@@ -77,30 +102,36 @@ bluebird.map(
         return googleMapsClient.directions({
             origin: config.origin,
             destination: ll,
-            alternatives: true
+            alternatives: false
         }).asPromise()
     }
 ).then(data => {
     console.log("plotting");
-    data.forEach(response => doSomething(dataToPlot, response.json));
+    data.forEach(response => addDirectionsToPlot(dataToPlot, response.json));
 
     var bounds = getBounds(dataToPlot);
     console.log("bounds: ", bounds);
 
-    dataToPlot.forEach(segment => {
-        pointScale(segment.start, bounds, config.desiredBounds);
-        pointScale(segment.end, bounds, config.desiredBounds);
+    dataToPlot.forEach(chain => {
+        chain.forEach(segment => {
+            pointScale(segment.start, bounds, config.desiredBounds);
+            pointScale(segment.end, bounds, config.desiredBounds);
+        });
     });
     console.log("newbounds: ", getBounds(dataToPlot));
 
     var model = [];
-    dataToPlot.forEach(segment => {
-        var cylinder = new CSG.roundedCylinder({
-            start: segment.start,
-            end: segment.end,
-            radius: config.printRadius / 2
-        });
-        model.push(cylinder);
+    dataToPlot.forEach(chain => {
+
+
+        chain.forEach(segment => {
+            var cylinder = new CSG.roundedCylinder({
+                start: segment.start,
+                end: segment.end,
+                radius: config.printRadius / 2
+            });
+            model.push(cylinder);
+        })
     });
     jscad.renderFile(model, 'output.stl')
 });
